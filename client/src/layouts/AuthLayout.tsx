4501c7c7b2ab73fe640da9fac4a72fe71c3280dd
DmitryVasylenko0808 <dvasilenko1999@gmail.com>
2024-01-13 16:20:31 +0300

    add logo on auth pages

--- client/src/layouts/AuthLayout.tsx
+++ client/src/layouts/AuthLayout.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router";
+import Logo from "../modules/Header/components/Logo";
 
 const AuthLayout = () => {
   return (
-    <div className="relative min-h-screen flex justify-center items-center">
+    <div className="relative min-h-screen flex flex-col justify-center items-center">
+      <Logo variant="auth" />
       <Outlet />
     </div>
   );
--- client/src/modules/Header/components/Logo.tsx
+++ client/src/modules/Header/components/Logo.tsx
@@ -3,7 +3,20 @@ import { Link } from "react-router-dom";
 
 import RecipeIcon from "../../../assets/icons/recipe.svg";
 
-const Logo = () => {
+type LogoProps = {
+  variant?: "default" | "auth";
+};
+
+const Logo: React.FC<LogoProps> = ({ variant = "default" }) => {
+  if (variant === "auth") {
+    return (
+      <Link to="/" className="mb-7 text-center flex items-center gap-x-2">
+        <RecipeIcon width={45} height={45} fill="#d14d72" />
+        <h1 className="text-3xl font-bold text-red">RecipesApp</h1>
+      </Link>
+    );
+  }
+
   return (
     <Link to="/" className="flex items-center gap-x-2">
       <RecipeIcon width={45} height={45} fill="white" />
